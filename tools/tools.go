57091666a1ea9aab3f56d4d0e452e60ce877c494
Ronny Orot <ronny.orot@gmail.com>
2024-12-25 14:10:13 +0200

    Update copyright headers
    Signed-off-by: Ronny Orot <ronny.orot@gmail.com>

--- tools/tools.go
+++ tools/tools.go
@@ -1,6 +1,9 @@
+// Copyright (c) The OpenTofu Authors
+// SPDX-License-Identifier: MPL-2.0
 // Copyright (c) HashiCorp, Inc.
 // SPDX-License-Identifier: MPL-2.0
 
+//go:build tools
 // +build tools
 
 package tools
