d7433eb2a73c0ad889de2759451eea921791f588
Andrei Ciobanu <andrei.ciobanu@opentofu.org>
2026-04-22 17:43:42 +0300

    Update test to have a more minimal example
    Signed-off-by: Andrei Ciobanu <andrei.ciobanu@opentofu.org>

--- internal/lsp/token_encoder_test.go
+++ internal/lsp/token_encoder_test.go
@@ -386,168 +386,133 @@ func TestTokenEncoder_unsupported(t *testing.T) {
 	}
 }
 
-func TestTokenEncoder_multiLineTokensWithChildTokens(t *testing.T) {
+func TestTokenEncoder_multiLineLocalWithInnerTokens(t *testing.T) {
 	// Reproduces https://github.com/opentofu/tofu-ls/issues/156
-	bytes := []byte(`variable "a" {
-  default = "hello"
-}
-
-variable "b" {
-  default = "world"
-}
-
-locals {
+	bytes := []byte(`locals {
+  a    = "test"
   test = <<-EOT
-    a: ${var.a}
-    b: ${var.b}
+    x: ${local.a}
+    y: ${local.a}
   EOT
-}`)
+}
+`)
 	// The TokenEncoder configured here simulates the result that the handlers/semantic_token.go would generate.
 	te := &TokenEncoder{
 		Lines: source.MakeSourceLines("test.tf", bytes),
 		Tokens: []lang.SemanticToken{
 			{
+				// The start of the locals block: locals
 				Type:      lang.TokenBlockType,
-				Modifiers: lang.SemanticTokenModifiers{lang.SemanticTokenModifier("opentofu-variable")},
+				Modifiers: lang.SemanticTokenModifiers{lang.SemanticTokenModifier("opentofu-locals")},
 				Range: hcl.Range{
 					Filename: "test.tf",
 					Start:    hcl.Pos{Line: 1, Column: 1, Byte: 0},
-					End:      hcl.Pos{Line: 1, Column: 9, Byte: 8},
+					End:      hcl.Pos{Line: 1, Column: 7, Byte: 6},
 				},
 			},
 			{
-				Type:      lang.TokenBlockLabel,
-				Modifiers: lang.SemanticTokenModifiers{lang.SemanticTokenModifier("opentofu-variable")},
-				Range: hcl.Range{
-					Filename: "test.tf",
-					Start:    hcl.Pos{Line: 1, Column: 10, Byte: 9},
-					End:      hcl.Pos{Line: 1, Column: 13, Byte: 12},
-				},
-			},
-			{
-				Type:      lang.TokenAttrName,
-				Modifiers: lang.SemanticTokenModifiers{lang.SemanticTokenModifier("opentofu-variable")},
-				Range: hcl.Range{
-					Filename: "test.tf",
-					Start:    hcl.Pos{Line: 2, Column: 3, Byte: 17},
-					End:      hcl.Pos{Line: 2, Column: 10, Byte: 24},
-				},
-			},
-			{
-				Type: lang.TokenString,
-				Range: hcl.Range{
-					Filename: "test.tf",
-					Start:    hcl.Pos{Line: 2, Column: 13, Byte: 27},
-					End:      hcl.Pos{Line: 2, Column: 20, Byte: 34},
-				},
-			},
-			{
-				Type:      lang.TokenBlockType,
-				Modifiers: lang.SemanticTokenModifiers{lang.SemanticTokenModifier("opentofu-variable")},
-				Range: hcl.Range{
-					Filename: "test.tf",
-					Start:    hcl.Pos{Line: 5, Column: 1, Byte: 38},
-					End:      hcl.Pos{Line: 5, Column: 9, Byte: 46},
-				},
-			},
-			{
-				Type:      lang.TokenBlockLabel,
-				Modifiers: lang.SemanticTokenModifiers{lang.SemanticTokenModifier("opentofu-variable")},
-				Range: hcl.Range{
-					Filename: "test.tf",
-					Start:    hcl.Pos{Line: 5, Column: 10, Byte: 47},
-					End:      hcl.Pos{Line: 5, Column: 13, Byte: 50},
-				},
-			},
-			{
-				Type:      lang.TokenAttrName,
-				Modifiers: lang.SemanticTokenModifiers{lang.SemanticTokenModifier("opentofu-variable")},
-				Range: hcl.Range{
-					Filename: "test.tf",
-					Start:    hcl.Pos{Line: 6, Column: 3, Byte: 55},
-					End:      hcl.Pos{Line: 6, Column: 10, Byte: 62},
-				},
-			},
-			{
-				Type: lang.TokenString,
-				Range: hcl.Range{
-					Filename: "test.tf",
-					Start:    hcl.Pos{Line: 6, Column: 13, Byte: 65},
-					End:      hcl.Pos{Line: 6, Column: 20, Byte: 72},
-				},
-			},
-			{
-				Type:      lang.TokenBlockType,
-				Modifiers: lang.SemanticTokenModifiers{lang.SemanticTokenModifier("opentofu-locals")},
-				Range: hcl.Range{
-					Filename: "test.tf",
-					Start:    hcl.Pos{Line: 9, Column: 1, Byte: 76},
-					End:      hcl.Pos{Line: 9, Column: 7, Byte: 82},
-				},
-			},
-			{
+				// The first local name: a
 				Type:      lang.TokenAttrName,
 				Modifiers: lang.SemanticTokenModifiers{lang.SemanticTokenModifier("opentofu-locals")},
 				Range: hcl.Range{
 					Filename: "test.tf",
-					Start:    hcl.Pos{Line: 10, Column: 3, Byte: 87},
-					End:      hcl.Pos{Line: 10, Column: 7, Byte: 91},
+					Start:    hcl.Pos{Line: 2, Column: 3, Byte: 11},
+					End:      hcl.Pos{Line: 2, Column: 4, Byte: 12},
 				},
 			},
 			{
+				// The value of the first local: "test"
 				Type: lang.TokenString,
 				Range: hcl.Range{
 					Filename: "test.tf",
-					Start:    hcl.Pos{Line: 11, Column: 5, Byte: 105},
-					End:      hcl.Pos{Line: 11, Column: 8, Byte: 108},
+					Start:    hcl.Pos{Line: 2, Column: 10, Byte: 18},
+					End:      hcl.Pos{Line: 2, Column: 16, Byte: 24},
 				},
 			},
 			{
-				Type: lang.TokenReferenceStep,
+				// The second local name: test
+				Type:      lang.TokenAttrName,
+				Modifiers: lang.SemanticTokenModifiers{lang.SemanticTokenModifier("opentofu-locals")},
 				Range: hcl.Range{
 					Filename: "test.tf",
-					Start:    hcl.Pos{Line: 11, Column: 10, Byte: 110},
-					End:      hcl.Pos{Line: 11, Column: 13, Byte: 113},
+					Start:    hcl.Pos{Line: 3, Column: 3, Byte: 27},
+					End:      hcl.Pos{Line: 3, Column: 7, Byte: 31},
 				},
 			},
 			{
-				Type: lang.TokenReferenceStep,
-				Range: hcl.Range{
-					Filename: "test.tf",
-					Start:    hcl.Pos{Line: 11, Column: 14, Byte: 114},
-					End:      hcl.Pos{Line: 11, Column: 15, Byte: 115},
-				},
-			},
-			{
+				// The name of the first multiline variable: "x: "
 				Type: lang.TokenString,
 				Range: hcl.Range{
 					Filename: "test.tf",
-					Start:    hcl.Pos{Line: 11, Column: 16, Byte: 116},
-					End:      hcl.Pos{Line: 12, Column: 8, Byte: 124},
+					Start:    hcl.Pos{Line: 4, Column: 5, Byte: 45},
+					End:      hcl.Pos{Line: 4, Column: 8, Byte: 48},
 				},
 			},
 			{
+				// Reference to the locals namespace: local
 				Type: lang.TokenReferenceStep,
 				Range: hcl.Range{
 					Filename: "test.tf",
-					Start:    hcl.Pos{Line: 12, Column: 10, Byte: 126},
-					End:      hcl.Pos{Line: 12, Column: 13, Byte: 129},
+					Start:    hcl.Pos{Line: 4, Column: 10, Byte: 50},
+					End:      hcl.Pos{Line: 4, Column: 15, Byte: 55},
 				},
 			},
 			{
+				// Reference to the first local name: a
 				Type: lang.TokenReferenceStep,
 				Range: hcl.Range{
 					Filename: "test.tf",
-					Start:    hcl.Pos{Line: 12, Column: 14, Byte: 130},
-					End:      hcl.Pos{Line: 12, Column: 15, Byte: 131},
+					Start:    hcl.Pos{Line: 4, Column: 16, Byte: 56},
+					End:      hcl.Pos{Line: 4, Column: 17, Byte: 57},
 				},
 			},
 			{
+				// The name of the second multiline variable. Since this is a multiline string, the start line of this token is different
+				// from the end line so this ends right after the first variable inside the heredoc and ends before the next reference
+				// to the "locals" namespace.
+				// Start indicated by "[" and stop by "]":
+				// test = <<-EOT
+				//  x: ${local.a}[
+				//  y: ${]local.a}
+				// EOT
 				Type: lang.TokenString,
 				Range: hcl.Range{
 					Filename: "test.tf",
-					Start:    hcl.Pos{Line: 12, Column: 16, Byte: 132},
-					End:      hcl.Pos{Line: 13, Column: 1, Byte: 133},
+					Start:    hcl.Pos{Line: 4, Column: 18, Byte: 58},
+					End:      hcl.Pos{Line: 5, Column: 8, Byte: 66},
+				},
+			},
+			{
+				// Reference to the locals namespace: local
+				Type: lang.TokenReferenceStep,
+				Range: hcl.Range{
+					Filename: "test.tf",
+					Start:    hcl.Pos{Line: 5, Column: 10, Byte: 68},
+					End:      hcl.Pos{Line: 5, Column: 15, Byte: 73},
+				},
+			},
+			{
+				// Reference to the first local name: a
+				Type: lang.TokenReferenceStep,
+				Range: hcl.Range{
+					Filename: "test.tf",
+					Start:    hcl.Pos{Line: 5, Column: 16, Byte: 74},
+					End:      hcl.Pos{Line: 5, Column: 17, Byte: 75},
+				},
+			},
+			{
+				// The remainder of the heredoc bytes. Since this is a multiline string, the start line of this token is different
+				// from the end line so this ends right after the second variable inside the heredoc and ends the heredoc ending
+				// Start indicated by "[" and stop by "]":
+				// test = <<-EOT
+				//  x: ${local.a}
+				//  y: ${local.a[}
+				// ]EOT
+				Type: lang.TokenString,
+				Range: hcl.Range{
+					Filename: "test.tf",
+					Start:    hcl.Pos{Line: 5, Column: 18, Byte: 76},
+					End:      hcl.Pos{Line: 6, Column: 1, Byte: 77},
 				},
 			},
 		},
@@ -559,33 +524,27 @@ locals {
 	data := te.Encode()
 
 	noModMask := uint32(TokenModifiersLegend(serverTokenModifiers.AsStrings()).BitMask(semtok.TokenModifiers{}))
-	varModMask := uint32(TokenModifiersLegend(serverTokenModifiers.AsStrings()).BitMask(semtok.TokenModifiers{"opentofu-variable"}))
 	localsModMask := uint32(TokenModifiersLegend(serverTokenModifiers.AsStrings()).BitMask(semtok.TokenModifiers{"opentofu-locals"}))
 	blockTypeIdx := uint32(TokenTypesLegend(serverTokenTypes.AsStrings()).Index(semtok.TokenType(lang.TokenBlockType)))
-	blockLabelIdx := uint32(TokenTypesLegend(serverTokenTypes.AsStrings()).Index(semtok.TokenType(lang.TokenBlockLabel)))
 	attrNameIdx := uint32(TokenTypesLegend(serverTokenTypes.AsStrings()).Index(semtok.TokenType(lang.TokenAttrName)))
 	strIdx := uint32(TokenTypesLegend(serverTokenTypes.AsStrings()).Index(semtok.TokenType(lang.TokenString)))
 	refIdx := uint32(TokenTypesLegend(serverTokenTypes.AsStrings()).Index(semtok.TokenType(lang.TokenReferenceStep)))
 
 	expectedData := []uint32{
-		0, 0, 8, blockTypeIdx, varModMask,
-		0, 9, 3, blockLabelIdx, varModMask,
-		1, 2, 7, attrNameIdx, varModMask,
-		0, 10, 7, strIdx, noModMask,
-		3, 0, 8, blockTypeIdx, varModMask,
-		0, 9, 3, blockLabelIdx, varModMask,
-		1, 2, 7, attrNameIdx, varModMask,
-		0, 10, 7, strIdx, noModMask,
-		3, 0, 6, blockTypeIdx, localsModMask,
+		0, 0, 6, blockTypeIdx, localsModMask,
+		1, 2, 1, attrNameIdx, localsModMask,
+		0, 7, 6, strIdx, noModMask,
 		1, 2, 4, attrNameIdx, localsModMask,
 		1, 4, 3, strIdx, noModMask,
-		0, 5, 3, refIdx, noModMask,
-		0, 4, 1, refIdx, noModMask,
-		0, 15, 15, strIdx, noModMask,
+		0, 5, 5, refIdx, noModMask,
+		0, 6, 1, refIdx, noModMask,
+		0, 17, 17, strIdx, noModMask,
 		1, 0, 7, strIdx, noModMask,
-		0, 4294967290, 3, refIdx, noModMask,
-		0, 4, 1, refIdx, noModMask,
-		0, 15, 15, strIdx, noModMask,
+		0, 9, 5, refIdx, noModMask,
+		// ^ The issue was here. The deltaStartChar (2nd value) was negative making neovim (>=0.12.0) running in an infinit loop.
+		// See the source code changed in the same commit with the addition of this test for more details
+		0, 6, 1, refIdx, noModMask,
+		0, 17, 17, strIdx, noModMask,
 		1, 0, 0, strIdx, noModMask,
 	}
 
